cc5f78f581d951f5cfd081572cb89a8d4653f7db
zanthbr <zanthblosmanis@gmail.com>
2020-02-23 02:19:52 +0000

    second commit

---
+++ if_statement.js
@@ -0,0 +1,8 @@
+let fruit = 'orange';
+let strlength = fruit.length;
+
+if (strlength > 5) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('The fruit name has 5 characters or less.');
+}
